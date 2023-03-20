0f1108e2008784a3afee5688038156e2948e5159
LiljayZZ <ayanbodejohnadeboye@gmail.com>
2023-03-20 09:49:29 +0100

    change Readme

---
+++ index.js
@@ -0,0 +1,34 @@
+let come = "30 Days Of Javascript"
+console.log(come);
+console.log(come.length);
+console.log(come.toUpperCase());
+console.log(come.toLowerCase());
+console.log(come.substring(0, 4));
+console.log(come.slice(0, 2));
+console.log(come.includes("java"));
+console.log(come.split(""));
+console.log(come.split(" "));
+let companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(companies.split(","));
+console.log(come.replace("Javascript", "Python"));
+let string = "30 Days Of Javascript"
+console.log(string.charAt(15));
+console.log(string.charCodeAt("J"));
+console.log(string.indexOf("a"));
+console.log(string.lastIndexOf("a"));
+let go = "You cannot end a sentence with because because because is a conjunction"
+console.log(go.indexOf("because"));
+console.log(go.lastIndexOf("because"));
+console.log(go.search("because"));
+let a = "  30 Days Of Javascript  "
+console.log(a.trim("30 Days Of Javascript"));
+console.log(string.startsWith("3"));
+console.log(string.endsWith("t"));
+console.log(string.match("a"));
+let strings = "30";
+console.log(strings.concat("Days", "Of", "and", "JavaScript"));
+console.log(string.repeat(2));
+let b = "There is no exercise better for the heart than reaching down and lifting people up."
+console.log(b);
+let c = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log(c)
